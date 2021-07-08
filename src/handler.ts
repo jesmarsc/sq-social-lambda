764ef4285c343878f0e8978755c56fef2537cb45
Jesmar Castillo <jesmar@ucsb.edu>
2021-07-08 09:12:01 -0700

    Cache element when image loaded

--- src/handler.ts
+++ src/handler.ts
@@ -2,11 +2,7 @@ import { APIGatewayProxyEvent } from 'aws-lambda';
 import { performance } from 'perf_hooks';
 import { fabric } from 'fabric';
 
-import {
-  cloneObject,
-  loadFabricImage,
-  loadFabricImageLocal,
-} from './templates/utils';
+import { loadFabricImage, loadFabricImageLocal } from './templates/utils';
 
 import { baseUrl } from './constants/url';
 import series1 from './series/series1-data.json';
@@ -17,6 +13,7 @@ import UbuntuBold from './fonts/Ubuntu-Bold.ttf';
 
 import questCompleteBackground from './assets/quest-complete.png';
 import xlmCoin from './assets/xlm_coin.png';
+import badge from './assets/badge.png';
 
 // @ts-ignore: Missing types
 fabric.nodeCanvas.registerFont(UbuntuBold, {
@@ -35,29 +32,60 @@ const pluralSuffix: { [key: string]: string } = {
   other: 'th',
 };
 
-let backgroundImage: fabric.Image;
-let badgeImageCache: { [publicKey: string]: string } = {};
-let coinImageCache: string;
+const textOptions = {
+  fontFamily: 'Ubuntu',
+  fontWeight: 'bold',
+  fontSize: 22,
+  fill: 'white',
+  shadow: '1 1 2',
+  originX: 'center',
+};
 
-const generateCompletionImage = async (
-  set: number,
-  quest: number,
-  position?: number
-) => {
+const imageElementCache: { [imageSrc: string]: HTMLImageElement } = {};
+
+const getElement = async (src: string) => {
+  if (!imageElementCache[src]) {
+    const image = await loadFabricImageLocal(src);
+    imageElementCache[src] = image.getElement() as HTMLImageElement;
+  }
+
+  return imageElementCache[src];
+};
+
+// let badgeImageCache: { [publicKey: string]: string } = {};
+
+const generateCompletionImage = async (params: {
+  set: number;
+  quest: number;
+  position?: number;
+}) => {
+  const { set, quest, position } = params;
+
+  if (!set || !quest) {
+    throw { status: 400, message: 'Set or quest query parameter is missing.' };
+  }
+
   const series = seriesData[set - 1];
-  const badgeIssuer = series.challenges[quest - 1].badges.main;
+  if (!series) throw { status: 404, message: 'Series not found.' };
 
-  if (!backgroundImage) {
-    backgroundImage = await loadFabricImageLocal(questCompleteBackground);
+  const badgeIssuer = series?.challenges[quest - 1]?.badges.main;
+  if (!badgeIssuer) throw { status: 404, message: 'Quest not found.' };
+
+  if (position && (position <= 0 || position > series.prizes.length)) {
+    throw { status: 400, message: 'Position not within prizes range.' };
   }
 
-  if (!badgeImageCache[badgeIssuer]) {
-    const badge = await loadFabricImage(
-      `${baseUrl}/badge/${badgeIssuer}?v=${set}`
-    );
-    badge.scaleToWidth(64);
-    badgeImageCache[badgeIssuer] = badge.toDataURL({});
-  }
+  // if (!badgeImageCache[badgeIssuer]) {
+  //   const badge = await loadFabricImage(
+  //     `${baseUrl}/badge/${badgeIssuer}?v=${set}`
+  //   );
+  //   badge.scaleToWidth(64);
+  //   badgeImageCache[badgeIssuer] = badge.toDataURL({});
+  // }
+
+  const backgroundImage = new fabric.Image(
+    await getElement(questCompleteBackground)
+  );
 
   const canvas = new fabric.StaticCanvas(null, {
     width: backgroundImage.width,
@@ -65,20 +93,15 @@ const generateCompletionImage = async (
     renderOnAddRemove: false,
   });
 
-  const imageText = new fabric.Text('', {
-    fontFamily: 'Ubuntu',
-    fontWeight: 'bold',
-    fontSize: 22,
-    fill: 'white',
-    shadow: '1 1 2',
+  const badgeImage = new fabric.Image(await getElement(badge), {
     originX: 'center',
   });
+  badgeImage.scaleToWidth(64);
 
-  const badgeImage = await loadFabricImage(badgeImageCache[badgeIssuer]);
-  badgeImage.set({ originX: 'center' });
-
-  const badgeText = await cloneObject(imageText);
-  badgeText.set({ text: '+1 NFT', top: badgeImage.height! + 8 });
+  const badgeText = new fabric.Text('+1 NFT', {
+    ...textOptions,
+    top: badgeImage.getScaledHeight() + 8,
+  });
 
   const badgeGroup = new fabric.Group([badgeImage, badgeText]);
 
@@ -86,40 +109,34 @@ const generateCompletionImage = async (
     padding: 8,
   });
 
-  if (position && series.prizes.length > 0) {
-    if (!coinImageCache) {
-      const coin = await loadFabricImageLocal(xlmCoin);
-      coin.scaleToWidth(64);
-      coinImageCache = coin.toDataURL({});
-    }
+  if (position) {
+    const coinImage = new fabric.Image(await getElement(xlmCoin), {
+      originX: 'center',
+    });
+    coinImage.scaleToWidth(64);
 
-    const coinImage = await loadFabricImage(coinImageCache);
-    coinImage.set({ originX: 'center' });
-
-    const coinText = await cloneObject(imageText);
-    coinText.set({
-      text: `+${series.prizes[position - 1] || 0} XLM`,
-      top: badgeImage.getScaledHeight() + 8,
+    const coinText = new fabric.Text(`+${series.prizes[position - 1]} XLM`, {
+      ...textOptions,
+      top: coinImage.getScaledHeight() + 8,
     });
 
-    const coinGroup = new fabric.Group([coinImage, coinText], {
-      top: rewards.get('top'),
-      left: rewards.get('left')! + 118,
-    });
+    const coinGroup = new fabric.Group([coinImage, coinText], { left: 96 });
 
     rewards.addWithUpdate(coinGroup);
 
-    const positionText = await cloneObject(imageText);
     const positionString = position.toString();
     const positionSuffix =
       pluralSuffix[pluralRules.select(position).toString()];
 
-    positionText.set({
-      top: rewards.get('top')! - 8,
-      left: rewards.getCenterPoint().x,
-      originY: 'bottom',
-      text: `${positionString}${positionSuffix} Place`,
-    });
+    const positionText = new fabric.Text(
+      `${positionString}${positionSuffix} Place`,
+      {
+        ...textOptions,
+        originY: 'bottom',
+        top: -4,
+        left: rewards.getCenterPoint().x,
+      }
+    );
 
     positionText.setSuperscript(
       positionString.length,
@@ -153,6 +170,7 @@ const generateCompletionImage = async (
   });
   rewards.centerH();
 
+  /* ~40ms */
   const image = canvas.toDataURL().replace(/^data:image\/png;base64,/, '');
 
   const response = {
@@ -168,15 +186,34 @@ const generateCompletionImage = async (
 };
 
 const handler = async (event: APIGatewayProxyEvent) => {
-  const { queryStringParameters } = event;
-  const { set, quest, position } = queryStringParameters as any;
+  try {
+    const { queryStringParameters } = event;
 
-  const time1 = performance.now();
-  const response = await generateCompletionImage(set, quest, position);
-  const time2 = performance.now();
-  console.log('execution time:', time2 - time1);
+    const execTime = performance.now();
+    const response = await generateCompletionImage(
+      queryStringParameters as any
+    );
+    console.log(performance.now() - execTime);
 
-  return response;
+    return response;
+  } catch (error: any) {
+    let message = error?.message || 'Request failed.';
+
+    if (typeof error === 'string') message = error;
+
+    const statusCode = error?.status || 400;
+
+    return {
+      statusCode,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        status: statusCode,
+        message,
+      }),
+    };
+  }
 };
 
 export default handler;
--- src/templates/utils.ts
+++ src/templates/utils.ts
@@ -1,12 +1,29 @@
 import { fabric } from 'fabric';
 
-export const loadFabricImage = (url: string) =>
-  new Promise<fabric.Image>((resolve) =>
-    fabric.Image.fromURL(url, (image) => resolve(image))
+export const loadFabricImage = (
+  url: string,
+  options?: fabric.IImageOptions
+) => {
+  return new Promise<fabric.Image>((resolve, reject) =>
+    fabric.Image.fromURL(
+      url,
+      (image) => {
+        // Fabric ignores error handling so just check any property to see if an image was successfully loaded.
+        // image._element will be null if not successful.
+        if (!image.getElement().src) {
+          reject({ message: 'Failed to load image.', statusCode: 404 });
+        }
+        resolve(image);
+      },
+      options
+    )
   );
+};
 
-export const loadFabricImageLocal = (path: string) =>
-  loadFabricImage(`file://${__dirname}/../${path}`);
+export const loadFabricImageLocal = (
+  path: string,
+  options?: fabric.IImageOptions
+) => loadFabricImage(`file://${__dirname}/../${path}`, options);
 
 export const cloneObject = <Type extends fabric.Object>(object: Type) =>
   new Promise<Type>((resolve) => object.clone((clone: Type) => resolve(clone)));
